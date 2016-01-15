373e6c440758db8fb9597bc9a890a2f6dc83bd36
shminer <a332574643@gmail.com>
2016-01-15 16:52:08 +0800

    update ramdisk to STOCK MM

--- init.lge.usb.sh
+++ init.lge.usb.sh
@@ -1,40 +1,62 @@
 #!/system/bin/sh
-# Copyright (c) 2012, The Linux Foundation. All rights reserved.
+
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above
-#       copyright notice, this list of conditions and the following
-#       disclaimer in the documentation and/or other materials provided
-#       with the distribution.
-#     * Neither the name of The Linux Foundation nor the names of its
-#       contributors may be used to endorse or promote products derived
-#      from this software without specific prior written permission.
+# Allow USB enumeration with default PID/VID
 #
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-#
+echo 1  > /sys/class/android_usb/f_mass_storage/lun/nofua
+echo 1  > /sys/class/android_usb/f_cdrom_storage/lun/nofua
+usb_config=`getprop persist.sys.usb.config`
+case "$usb_config" in
+    "" | "pc_suite" | "mtp_only" | "auto_conf")
+        setprop persist.sys.usb.config mtp
+        ;;
+    "adb" | "pc_suite,adb" | "mtp_only,adb" | "auto_conf,adb")
+        setprop persist.sys.usb.config mtp,adb
+        ;;
+    "ptp_only")
+        setprop persist.sys.usb.config ptp
+        ;;
+    "ptp_only,adb")
+        setprop persist.sys.usb.config ptp,adb
+        ;;
+    * ) ;; #USB persist config exists, do nothing
+esac
+
+################################################################################
+# QCOM
+################################################################################
+
 chown -h root.system /sys/devices/platform/msm_hsusb/gadget/wakeup
 chmod -h 220 /sys/devices/platform/msm_hsusb/gadget/wakeup
+
+# Target as specified in build.prop at compile-time
+target=`getprop ro.board.platform`
+
+# Set platform variables
+if [ -d /sys/devices/soc0 ]; then
+    soc_hwplatform=`cat /sys/devices/soc0/hw_platform` 2> /dev/null
+    soc_id=`cat /sys/devices/soc0/soc_id` 2> /dev/null
+else
+    soc_hwplatform=`cat /sys/devices/system/soc/soc0/hw_platform` 2> /dev/null
+fi
+
+#
+# soc_id may specify additional chip variants not captured in ro.board.platform
+# so allow for additional target differentiation based on that
+#
+case $soc_id in
+    "252")
+        target="apq8092"
+    ;;
+    "253")
+        target="apq8094"
+    ;;
+esac
 
 #
 # Allow persistent usb charging disabling
 # User needs to set usb charging disabled in persist.usb.chgdisabled
 #
-target=`getprop ro.board.platform`
 usbchgdisabled=`getprop persist.usb.chgdisabled`
 case "$usbchgdisabled" in
     "") ;; #Do nothing here
@@ -43,10 +65,10 @@ case "$usbchgdisabled" in
         "msm8660")
         echo "$usbchgdisabled" > /sys/module/pmic8058_charger/parameters/disabled
         echo "$usbchgdisabled" > /sys/module/smb137b/parameters/disabled
-    ;;
+        ;;
         "msm8960")
         echo "$usbchgdisabled" > /sys/module/pm8921_charger/parameters/disabled
-    ;;
+        ;;
     esac
 esac
 
@@ -57,7 +79,7 @@ case "$usbcurrentlimit" in
     case $target in
         "msm8960")
         echo "$usbcurrentlimit" > /sys/module/pm8921_charger/parameters/usb_max_current
-    ;;
+        ;;
     esac
 esac
 
@@ -78,53 +100,10 @@ for f in /sys/bus/esoc/devices/*; do
 done
 fi
 
-target=`getprop ro.product.device`
-cdromenable=`getprop persist.service.cdrom.enable`
-create_luns() {
-    case "$1" in
-        "msm8226" | "msm8610")
-        case "$2" in
-            0)
-                setprop persist.sys.usb.luns ""
-            ;;
-            1)
-                setprop persist.sys.usb.luns rom
-            ;;
-        esac
-        ;;
-    esac
-}
-
-#
-# Allow USB enumeration with default PID/VID
-#
-baseband=`getprop ro.baseband`
-echo 1  > /sys/class/android_usb/f_mass_storage/lun/nofua
-echo 1 > /sys/class/android_usb/f_cdrom_storage/lun/nofua
-usb_config=`getprop persist.sys.usb.config`
-case "$usb_config" in
-    #USB persist config not set, select default configuration
-    "")
-        setprop persist.sys.usb.config auto_conf
-    ;;
-    "adb")
-        setprop persist.sys.usb.config auto_conf,adb
-    ;;
-    *)
-    ;; #USB persist config exists, do nothing
-esac
-
-diag_enable=`getprop persist.sys.usb.diag_enable`
-case "$diag_enable" in
-    "1")
-        echo 1 > /sys/devices/platform/lg_diag_cmd/diag_enable
-    ;;
-    *) ;; #Do nothing here
-esac
-
 #
 # Do target specific things
 #
+baseband=`getprop ro.baseband`
 case "$target" in
     "msm8974")
 # Select USB BAM - 2.0 or 3.0
@@ -134,11 +113,7 @@ case "$target" in
         if [ "$baseband" == "apq" ]; then
             echo "msm_hsic_host" > /sys/bus/platform/drivers/xhci_msm_hsic/unbind
         fi
-
-        if [ "$esoc_link" == "PCIe" ] || [ "$esoc_link" == "HSIC+PCIe" ]; then
-            echo 2 > /sys/module/g_android/parameters/tx_qmult
-            echo 1036800 > /sys/module/g_android/parameters/min_cpu_freq
-        fi
+        echo qti,ether > /sys/class/android_usb/android0/f_rmnet/transports
     ;;
     "msm8226")
          if [ -e /sys/bus/platform/drivers/msm_hsic_host ]; then
@@ -146,6 +121,25 @@ case "$target" in
                  echo msm_hsic_host > /sys/bus/platform/drivers/msm_hsic_host/unbind
              fi
          fi
+    ;;
+    "msm8994" | "msm8992" | "msm8996")
+        echo BAM2BAM_IPA > /sys/class/android_usb/android0/f_rndis_qc/rndis_transports
+        echo qti,bam2bam_ipa > /sys/class/android_usb/android0/f_rmnet/transports
+    ;;
+    "msm8952" | "msm8976")
+        echo BAM2BAM_IPA > /sys/class/android_usb/android0/f_rndis_qc/rndis_transports
+        # Increase RNDIS DL max aggregation size to 11K
+        echo 11264 > /sys/module/g_android/parameters/rndis_dl_max_xfer_size
+        echo qti,bam2bam_ipa > /sys/class/android_usb/android0/f_rmnet/transports
+    ;;
+    "apq8064")
+        echo hsic,hsic > /sys/class/android_usb/android0/f_rmnet/transports
+    ;;
+    "msm8909")
+    echo qti,bam > /sys/class/android_usb/android0/f_rmnet/transports
+    ;;
+    * )
+        echo smd,bam > /sys/class/android_usb/android0/f_rmnet/transports
     ;;
 esac
 
@@ -191,3 +185,12 @@ case "$baseband" in
           chown -h radio.radio /sys/devices/virtual/hsicctl/hsicctl0/modem_wait
     ;;
 esac
+
+################################################################################
+# DEVICE
+################################################################################
+
+if [ -f "/init.lge.usb.dev.sh" ]
+then
+    source /init.lge.usb.dev.sh
+fi
